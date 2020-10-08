26d2c49d1a2815e1d1e464bb88d4501880512eff
Matheus Tavares <s0berano@pop-os.localdomain>
2020-10-08 11:22:01 -0400

    feat: threading process

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from app.controller.cptec import cptec
 from app.controller.covid import covid
+import threading
 
+cptec = threading.Thread(target=cptec)
+covid = threading.Thread(target=covid)
 
-report = {
-    'cptec' : cptec,
-    'covid' : covid
-}
+def run(): 
+    cptec.start()
+    covid.start()
